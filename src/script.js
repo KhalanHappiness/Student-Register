5c28dead9ab592d35be8c0bb68173b0788ae80ec
happiness <happinesskhalan@gmail.com>
2025-03-27 17:50:53 +0300

    Added DOM references in JavaScript for easier element access

--- src/script.js
+++ src/script.js
@@ -2,7 +2,13 @@ document.addEventListener("DOMContentLoaded", function(){
 
     //DOM Element References
 
-    
+    const search = document.getElementById("search")
+    const searchButton = document.getElementById("searchButton")
+    const form = document.getElementById("register")
+    const name = document.getElementById("name")
+    const className = document.getElementById("className")
+    const total = document.getElementById("totals")
+    const results = document.getElementById("results")
 
 
 
