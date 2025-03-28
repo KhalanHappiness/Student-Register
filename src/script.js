cd499f0a032ff9a848e4bef5f91e0399e4bc32c9
happiness <happinesskhalan@gmail.com>
2025-03-28 12:36:38 +0300

    created the renderStudentRow function

--- src/script.js
+++ src/script.js
@@ -40,7 +40,7 @@ document.addEventListener("DOMContentLoaded", function(){
             const search = document.getElementById("search").value.trim()
          
 
-            fetch(`http://localhost:3000/students/${search}`,{
+            fetch(`http://localhost:3000/students/?id=${search}`,{
 
                 headers:{
                     "Content-Type": "application/json",
@@ -56,43 +56,18 @@ document.addEventListener("DOMContentLoaded", function(){
 
                 // Clear previous search results
                 tableBody.innerHTML = "";
-               
-
-                //loop through student data
-                students.forEach(student => {
-
-                    let row = document.createElement("tr")
-
-                    //create and append id cell
-                    let idCell = document.createElement("td")
-                    idCell.textContent = student.id
-                    row.appendChild(idCell)
-
-                    //create and append name cell
-                    let nameCell = document.createElement("td")
-                    nameCell.textContent = student.name
-                    row.appendChild(nameCell)
-
-                     //create and append class cell
-                     let classCell = document.createElement("td")
-                     classCell.textContent = student.class
-                     row.appendChild(classCell)
-
-                     //create and append status cell
-                     let statusCell = document.createElement("td")
-                    statusCell.createElement("button")
-
-                    //create and append row to the tbody
-                    tableBody.appendChild(row)
-
-                   
-                    
-                })
 
                 // If no student is found
                 if (students.length === 0) {
-                    tableBody.innerHTML = `<tr><td colspan="3">No student found</td></tr>`;
+                    tableBody.innerHTML = `<tr><td colspan="3">No student found</td></tr>`
+                    return
                 }
+               
+
+                //loop through student data
+                students.forEach(student => {renderStudentRow(student)})
+
+                
             })
 
             
@@ -114,58 +89,62 @@ document.addEventListener("DOMContentLoaded", function(){
 
     
 
-        students.forEach(student => {
-
-            let row = document.createElement("tr")
-
-            //create and append id cell
-            let idCell = document.createElement("td")
-            idCell.textContent = student.id
-            row.appendChild(idCell)
-
-            //create and append namecell
-            let nameCell = document.createElement("td")
-            nameCell.textContent = student.name
-            row.appendChild(nameCell)
-
-            //create and append class cell
-            let classCell = document.createElement("td")
-            classCell.textContent = student.class
-            row.appendChild(classCell)
-
-            //create and append the status cell
-
-            let statusCell = document.createElement("td")
-            //create the present button
-            let presentButton = document.createElement("button");
-            presentButton.textContent="present"
-            presentButton.classList.add("btn")
-            presentButton.addEventListener("click", function(){
-                markAttendance(student.id, "Present", presentButton, absentButton)
-            })
-            statusCell.appendChild(presentButton)
-
-           
-            //create the absent button
-            let absentButton = document.createElement("button");
-            absentButton.textContent = "Absent"
-            absentButton.classList.add("absent-btn")
-            absentButton.addEventListener('click', function(){
-                markAttendance(student.id, "Absent", presentButton, absentButton)
-            })
-            statusCell.appendChild(absentButton)
-           
-            row.appendChild(statusCell)
-
-
-            tableBody.appendChild(row)
-
-
-
-
-
-        })
+        students.forEach(student => {renderStudentRow(student)})
     }
+
+    //function to display rows
+
+    function renderStudentRow(student){
+
+        let row = document.createElement("tr")
+
+        //create and append id cell
+        let idCell = document.createElement("td")
+        idCell.textContent = student.id
+        row.appendChild(idCell)
+
+        //create and append namecell
+        let nameCell = document.createElement("td")
+        nameCell.textContent = student.name
+        row.appendChild(nameCell)
+
+        //create and append class cell
+        let classCell = document.createElement("td")
+        classCell.textContent = student.class
+        row.appendChild(classCell)
+
+        //create and append the status cell
+
+        let statusCell = document.createElement("td")
+        //create the present button
+        let presentButton = document.createElement("button");
+        presentButton.textContent="present"
+        presentButton.classList.add("btn")
+        presentButton.addEventListener("click", function(){
+            markAttendance(student.id, "Present", presentButton, absentButton)
+        })
+        statusCell.appendChild(presentButton)
+
+       
+        //create the absent button
+        let absentButton = document.createElement("button");
+        absentButton.textContent = "Absent"
+        absentButton.classList.add("absent-btn")
+        absentButton.addEventListener('click', function(){
+            markAttendance(student.id, "Absent", presentButton, absentButton)
+        })
+        statusCell.appendChild(absentButton)
+       
+        row.appendChild(statusCell)
+
+
+        tableBody.appendChild(row)
+
+
+
+    }
+
+
 
     //function to mark attendance(disable buttons after selection)
     function markAttendance(studentId, status, presentBtn, absentbtn){
